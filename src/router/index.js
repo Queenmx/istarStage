e2229a1d2f5ddcd3cb19dd8e9fa2edc55893bf69
1004606010@qq.com <mingxin@istarfinancial.cn>
2018-07-03 16:25:16 +0800

    update login

--- src/router/index.js
+++ src/router/index.js
@@ -149,9 +149,9 @@ const router = new Router({
     }
 })
 import { getItem, setItem } from "../util/util";
-setItem("userInfo",{'phone':'1232321321323'})
+// setItem("userInfo",{'phone':'1232321321323'})
 router.beforeEach((to, from, next) => {
-    if (to.path !== "/login" && !getItem("userInfo")) {
+    if (to.path !== "/login" && !getItem("api_token")) {
         // this route requires auth, check if logged in
         // if not, redirect to login page.
         next({
