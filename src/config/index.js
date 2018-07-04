f8866a467dbf0d85a4ae84cfa504ca390608dfe1
pengli <pengli@istarfinancial.cn>
2018-07-04 16:47:27 +0800

    忘记密码，设置密码，验证码登录，实名认证，注册联调

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,9 @@ let routerMode = "history";
 let imgBaseUrl = "http://xrjf.oss-cn-shanghai.aliyuncs.com/";
 const KEY = "JJPT@20200606@H5";
 if (process.env.NODE_ENV === "development") {
-    baseUrl = "http://whxr.free.ngrok.cc/";
+    // baseUrl = "http://whxr.free.ngrok.cc/";
+    baseUrl = "http://192.168.0.49:8888/";
+    
 } else if (process.env.NODE_ENV === "production") {
     baseUrl = "http://fsapi.starjrcs.com:8887/";//正式地址
 } else if (process.env.NODE_ENV === "testing") {
--- src/pages/setpsd.vue
+++ src/pages/setpsd.vue
@@ -9,17 +9,50 @@
             <van-field  type="password" placeholder="请再次输入密码"  v-model="repsd" />
         </van-cell-group>
         <div class="btnBox">
-            <van-button size="large" type="primary">确定</van-button>
+            <van-button size="large" type="primary" @click="confirm">确定</van-button>
         </div>
     </div>
 </template>
 <script>
+import { setPassword } from "@/util/axios";
+import {  setItem,getItem } from "@/util/util.js";
+
 export default {
   data() {
     return {
-      psd: "",
-      repsd: ""
+        userInfo:getItem("userInfo"),
+        psd: "",
+        repsd: ""
     };
+  },
+  methods: {
+    confirm(){
+        if(!/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,12}$/.test(this.psd)){//6-12位数字和字母
+            this.$toast("密码格式不正确");
+            return false;
+        }
+        if(this.psd!=this.repsd){
+            this.$toast("两次密码输入不一致");
+        }else{
+            this.setPassword();
+        }
+        
+    },
+    //设置密码接口
+    async setPassword(){
+        let data={
+            cusPhone:this.userInfo.cusPhone,
+            cusPassword:this.psd,
+        }
+        let res = await setPassword(data);
+        if(res.code==200){
+            this.$toast(res.msg); 
+            this.$router.push({ path: "/certification" });
+        }else{
+            this.$toast(res.msg); 
+        }
+          
+    },
   }
 };
 </script>
