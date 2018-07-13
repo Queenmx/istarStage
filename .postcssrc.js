05a629e5813e0c2d49cec2ec2a79853c1ca2199a
cancancan <994256595@qq.com>
2018-07-13 16:17:46 +0800

    update

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,24 +1,24 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-    plugins: [
-        require("postcss-import")({
-            extensions: ["pcss", "css"]
-        }),
-        require('postcss-theme-variables')({
-            vars: {
-                'button-primary-background-color': "#0074e4",
-                'button-primary-border-color': "#0074e4",
-                'button-disabled-color': '#fff',
-                'button-disabled-background-color': '#a1a1a1',
-                'button-disabled-border-color': '#a1a1a1',
-                'button-bottom-action-primary-background-color': "#0074e4"
-            },
-            prefix: '$'
-        }),
-        require("precss")(),
-        require("autoprefixer")({
-            browsers: ["Android >= 4.0", "iOS >= 7"]
-        })
-    ]
+  plugins: [
+    require("postcss-import")({
+      extensions: ["pcss", "css"]
+    }),
+    // require('postcss-theme-variables')({
+    //     vars: {
+    //         'button-primary-background-color': "#0074e4",
+    //         'button-primary-border-color': "#0074e4",
+    //         'button-disabled-color': '#fff',
+    //         'button-disabled-background-color': '#a1a1a1',
+    //         'button-disabled-border-color': '#a1a1a1',
+    //         'button-bottom-action-primary-background-color': "#0074e4"
+    //     },
+    //     prefix: '$'
+    // }),
+    require("precss")(),
+    require("autoprefixer")({
+      browsers: ["Android >= 4.0", "iOS >= 7"]
+    })
+  ]
 };
